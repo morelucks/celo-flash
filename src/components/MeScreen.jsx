f393c9055e197487cc2c719392c0d87ebf02dc8b
morelucks <luckykamshak@gmail.com>
2026-06-29 13:48:26 +0100

    Fix React strokeWidth warnings in MeScreen share button

--- src/components/MeScreen.jsx
+++ src/components/MeScreen.jsx
@@ -37,7 +37,7 @@ export default function MeScreen() {
             <span className="profile-fid">fid 1104338</span>
           </div>
           <button className="share-btn" onClick={handleShare}>
-            <svg className="share-icon" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2">
+            <svg className="share-icon" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2">
               <path d="M4 12v8a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2v-8"></path>
               <polyline points="16 6 12 2 8 6"></polyline>
               <line x1="12" y1="2" x2="12" y2="15"></line>
