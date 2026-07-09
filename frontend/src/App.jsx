bacb79c28abf9144eac26080762b29dcf34b7aeb
shaaibu7 <shaaibusuleiman9@gmail.com>
2026-07-09 19:38:33 +0100

    docs: add header bar description

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -74,7 +74,7 @@ function MainAppContent() {
     <div className="warpcast-desktop-bg">
       <div className="phone-container">
         
-        {/* Header Bar */}
+        {/* Header Bar - top navigation */}
         <Header onOpenSwap={() => setSwapOpen(true)} />
 
         {/* Rolling News Marquee */}
