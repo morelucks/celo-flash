ec5d65af0932a4ec16bb01fbc985aa9991f1b796
morelucks <luckykamshak@gmail.com>
2026-07-10 23:47:12 +0100

    refactor: clean up comments in CeloFlashSavings.sol

--- contract/contracts/CeloFlashSavings.sol
+++ contract/contracts/CeloFlashSavings.sol
@@ -376,3 +376,5 @@ contract CeloFlashSavings is ReentrancyGuard, Ownable, Pausable {
 // Commit 23: docs: document MockAavePool withdraw mechanism
 
 // Commit 24: docs: explain MockAavePool minting fallback for yield simulation
+
+// Commit 25: refactor: clean up comments in CeloFlashSavings.sol
