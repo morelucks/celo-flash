3bea2cb77704a2c987ae33dc363b1046a28f8b60
morelucks <luckykamshak@gmail.com>
2026-07-10 23:47:12 +0100

    docs: add NatSpec comments to rescueTokens function

--- contract/contracts/CeloFlashSavings.sol
+++ contract/contracts/CeloFlashSavings.sol
@@ -350,3 +350,5 @@ contract CeloFlashSavings is ReentrancyGuard, Ownable, Pausable {
 // Commit 10: docs: document Aave V3 integration design decisions
 
 // Commit 11: docs: document shares-based dynamic yield calculation method
+
+// Commit 12: docs: add NatSpec comments to rescueTokens function
