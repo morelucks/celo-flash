49d449bb591e277447ecae8b0d51df7c4c623dcc
shaaibu7 <shaaibusuleiman9@gmail.com>
2026-07-17 21:37:37 +0100

    test(tournament): add "CeloFlashTournament — Fee Withdrawal & Accounting" group

---
+++ contract/test/CeloFlashTournament.test.js
@@ -0,0 +1,6 @@
+const { expect } = require("chai");
+const { ethers } = require("hardhat");
+const { time } = require("@nomicfoundation/hardhat-network-helpers");
+
+describe("CeloFlashTournament — Fee Withdrawal & Accounting", function () {
+});
