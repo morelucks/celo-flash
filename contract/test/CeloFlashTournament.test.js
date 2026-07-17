9f2ecd4d14f12e9df29d2522432604edf31af731
shaaibu7 <shaaibusuleiman9@gmail.com>
2026-07-17 21:37:37 +0100

    test(tournament): add test constants

--- contract/test/CeloFlashTournament.test.js
+++ contract/test/CeloFlashTournament.test.js
@@ -3,4 +3,5 @@ const { ethers } = require("hardhat");
 const { time } = require("@nomicfoundation/hardhat-network-helpers");
 
 describe("CeloFlashTournament — Fee Withdrawal & Accounting", function () {
+  const ENTRY_FEE = ethers.parseEther("10");
 });
