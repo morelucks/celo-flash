7de85787e5b77d8ad9299b7a6ca1c50067714bc2
shaaibu7 <shaaibusuleiman9@gmail.com>
2026-07-17 21:37:37 +0100

    test(wager): add "CeloFlashWager — House Edge Withdrawal & Accounting" group

---
+++ contract/test/CeloFlashWager.test.js
@@ -0,0 +1,6 @@
+const { expect } = require("chai");
+const { ethers } = require("hardhat");
+const { time } = require("@nomicfoundation/hardhat-network-helpers");
+
+describe("CeloFlashWager — House Edge Withdrawal & Accounting", function () {
+});
